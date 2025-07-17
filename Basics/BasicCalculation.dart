76142c3b09ed38794b1679526c64020610197fe6
Saurabhgit157 <sy2961327@gmail.com>
2025-07-17 22:46:05 +0530

    basic calculation

---
+++ Basics/BasicCalculation.dart
@@ -0,0 +1,18 @@
+void main()
+{
+    int num1 = 10;//declaring number1
+    int num2 =20 ;//Declaring number2
+
+    //calculation
+    int sum = num1 + num2 ;
+    int diff = num1 - num2 ;
+    int mul= num1*num2;
+    double div = num1/num2 ;//it is double bcz its output is decimal.
+
+    //displaying output
+    print("the sum is $sum");
+    print("the difference is $difference");
+    print("the mul is $mul");
+    print("the div is $div");
+
+}
