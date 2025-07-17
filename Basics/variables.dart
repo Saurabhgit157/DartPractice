098e8447d5a5d0e72d64bb9fd9002c5b8780bc5b
Saurabhgit157 <sy2961327@gmail.com>
2025-07-18 03:46:09 +0530

    learn variables and variable naming convention

---
+++ Basics/variables.dart
@@ -0,0 +1,32 @@
+void main()
+{
+    //here variable name contains value Saurabh Is going to BE Billionaire
+    var name = "Saurabh is going to be Billionaire";
+    print(name);
+    String name1 = "Saurabh";
+    String address = "Bihar";
+    int age=20;
+    double height=5.5;
+    num numberPhone=111111123334;//used to store any type number
+    num ipAddress=2343234756.5324;
+    bool isMarried = false;
+
+    //print variables value
+    print("Name is $name");
+    print("Address is $address");
+    print("Age is $age");
+    print("height is $height");
+    print("Phone number is $numberPhone");
+    print("Married status is $isMarried");
+
+    //constant value which is never changed 
+    const pi =3.14;
+    
+    print("value of  PI is $pi");
+    //naving convention is lowercamle case for variables
+    //not standard way
+    var fullname="Saurabh Yadav";
+    var fullName="Saurabh Yadav";
+    
+
+}
