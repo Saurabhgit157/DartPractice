b71d9ff067d4a57bd8786b6bf124d1ebd7e9b039
Saurabh Yadav <sy2961327@gmail.com>
2025-07-17 15:34:32 +0530

    Initial commit : Dart Basic Practice

---
+++ Basics/HelloWorld.dart
@@ -0,0 +1,12 @@
+void main()
+{
+    //hello world
+    print("hello world");
+    //var keyword
+    var name = "Saurabh";
+    print(name);
+    //joining variable using $
+    var firstname = "Saurabh";
+    var lastname = "Yadav";
+    print("full name is $firstname $lastname");
+}
